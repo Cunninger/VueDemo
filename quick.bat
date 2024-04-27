7593cefeb834834e5227bd93b9fccbde0853e93a
Cunninger <1803912219@qq.com>
2024-04-27 17:47:54 +0800

    属性绑定

---
+++ quick.bat
@@ -0,0 +1,10 @@
+@echo off
+chcp 65001
+REM 添加所有改动到暂存区
+git add .
+
+REM 提交改动
+git commit -m "属性绑定"
+
+REM 推送改动到远程仓库
+git push origin master
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,7 @@ export default {
         }
     }
 }
-</script> -->
+</script>  -->
 
 
 <!-- <template>
@@ -36,10 +36,10 @@ export default {
 </style> -->
 
 
-
+<!-- 
 <template>
     <div>
-        <div :id="duId" :class="msg">测试</div>
+        <div :id="duId" :class="msg" :title="heelojsp">测试</div>
         <button @click="changeStyle">改变样式</button>
     </div>
 </template>
@@ -49,7 +49,8 @@ export default {
     data() {
         return {
             msg: "cc",
-            duId: "myDiv"
+            duId: "myDiv",
+            heelojsp: "这是一个div"
         }
     },
     methods: {
@@ -70,4 +71,73 @@ export default {
     color: blue;
     background-color: green;
 }
-</style>
+</style> -->
+
+
+<!-- <template>
+    <div>
+        <div :id="duId" :class="msg" :title="heelojsp">鼠标悬停看提示</div>
+        <button @click="changeTitle">改变提示</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: "cc",
+            duId: "myDiv",
+            heelojsp: "初始提示"
+        }
+    },
+    methods: {
+        changeTitle() {
+            this.heelojsp = this.heelojsp === '初始提示' ? '新的提示' : '初始提示';
+        }
+    }
+}
+</script>
+
+<style>
+.cc {
+    color: red;
+    background-color: yellow;
+}
+</style> -->
+
+
+<!-- <template>
+<div :class="union.msg":id="union.duId" :title="union.heelojsp">测测测</div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            union:{
+                msg: "cc",
+                duId: "myDiv",
+                heelojsp: "初始提示"
+            }
+        }
+    }
+}
+
+</script> -->
+
+
+<template>
+    <button :disabled="isButtionDisabled">
+        按钮
+    </button>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isButtionDisabled: false
+        }
+    }
+}
+</script>
