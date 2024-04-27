78c699c0a05abfb355743804c777ad756ea874c7
Cunninger <1803912219@qq.com>
2024-04-27 16:36:59 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div id ="app"></div>
+  <HelloWorld/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default{
+  name:'App',
+  components:{
+    HelloWorld
+  }
+
+}
+
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,73 @@
+<!-- <template>
+    <div class="{{ msg }}">测试</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: "active "
+        }
+    }
+}
+</script> -->
+
+
+<!-- <template>
+    <div :id="duId" :class="msg">测试</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: "cc",
+            duId: "myDiv"
+        }
+    }
+}
+</script>
+
+<style>
+.cc {
+    color: red;
+    background-color: yellow;
+}
+</style> -->
+
+
+
+<template>
+    <div>
+        <div :id="duId" :class="msg">测试</div>
+        <button @click="changeStyle">改变样式</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: "cc",
+            duId: "myDiv"
+        }
+    },
+    methods: {
+        changeStyle() {
+            this.msg = this.msg === 'cc' ? 'dd' : 'cc';
+        }
+    }
+}
+</script>
+
+<style>
+.cc {
+    color: red;
+    background-color: yellow;
+}
+
+.dd {
+    color: blue;
+    background-color: green;
+}
+</style>
